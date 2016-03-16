4fec37b5804e654c58d5b85e19e444dcf284d461
Bill McDowell <forkunited@gmail.com>
2016-03-16 03:26:51 -0400

    ...

--- src/main/java/edu/cmu/ml/rtw/generic/task/classify/TaskClassification.java
+++ src/main/java/edu/cmu/ml/rtw/generic/task/classify/TaskClassification.java
@@ -89,11 +89,9 @@ public class TaskClassification<D extends Datum<L>, L> extends CtxParsableFuncti
 				
 				if (actual.equals(predicted)) {
 					incrementStat(stats, predicted, Stat.TRUE_POSITIVE, count);
-					if (predicted != null) {
-						for (L label : actualToPredicted.keySet())
-							if (!label.equals(predicted))
-								incrementStat(stats, label, Stat.TRUE_NEGATIVE, count);
-					}
+					for (L label : actualToPredicted.keySet())
+						if (!label.equals(predicted))
+							incrementStat(stats, label, Stat.TRUE_NEGATIVE, count);
 				} else {
 					if (predicted != null)
 						incrementStat(stats, predicted, Stat.FALSE_POSITIVE, count);
