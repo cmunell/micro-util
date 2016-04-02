a22dfe45417b0d7aab43cb069fe81d64666f7314
Bill McDowell <forkunited@gmail.com>
2016-04-01 21:57:29 -0400

    ...

--- src/main/java/edu/cmu/ml/rtw/generic/model/annotator/nlp/mateplus/MatePlus.java
+++ src/main/java/edu/cmu/ml/rtw/generic/model/annotator/nlp/mateplus/MatePlus.java
@@ -28,7 +28,7 @@ public class MatePlus {
 		String taggerModel = "/data_reitter/nlp_tools/mateplus/models/CoNLL2009-ST-English-ALL.anna-3.3.postagger.model";
 		String srlModel = "/data_reitter/nlp_tools/mateplus/models/CoNLL2009-ST-English-ALL.anna-3.3.srl-4.1.srl.model";
 		//String srlModel = "/data_reitter/nlp_tools/mateplus/models/srl-EMNLP14+fs-eng.model";
-		String inputText = "John Anderson baked a cake.\n  Sally ate it for an excellent dinner.\n John frustratedly threw his fork at Sally.\n Jim flew his spaceship to Texas.\n Debra went to the store on Sunday.\n The game starts sometime on Sunday evening in Madrid.";
+		String inputText = "John Anderson baked a cake.\n  Sally ate it for an excellent dinner.\n John frustratedly threw his fork at Sally.\n Jim flew his spaceship to Texas.\n Debra went to the store on Sunday.\n The game starts sometime on Sunday evening in Madrid.\n I went on a flight through the air.";
 		run(lemmaModel, parserModel, taggerModel, srlModel, inputText);
 	}
 	
