d5bb6c3b7926b99f23152ff3f3ee81dab1518627
Bill McDowell <forkunited@gmail.com>
2016-04-17 23:57:31 -0400

    ...

--- src/main/java/edu/cmu/ml/rtw/generic/data/feature/fn/FnNGramContext.java
+++ src/main/java/edu/cmu/ml/rtw/generic/data/feature/fn/FnNGramContext.java
@@ -49,14 +49,14 @@ public class FnNGramContext extends FnNGram {
 	
 	@Override
 	protected boolean getNGrams(TokenSpan tokenSpan, Collection<TokenSpan> ngrams) {
-		if ((this.type != Type.AFTER) && (this.allowSqueeze || tokenSpan.getStartTokenIndex() - this.n >= 0)) {
+		if ((this.type != Type.AFTER && this.type != Type.AFTER_INCLUDING) && (this.allowSqueeze || tokenSpan.getStartTokenIndex() - this.n >= 0)) {
 			ngrams.add(new TokenSpan(tokenSpan.getDocument(), 
 									 tokenSpan.getSentenceIndex(), 
 									 Math.max(0, tokenSpan.getStartTokenIndex() - this.n), 
 									 tokenSpan.getStartTokenIndex() + ((this.type == Type.BEFORE || this.type == Type.BEFORE_AND_AFTER) ? 0 : tokenSpan.getLength())));
 		} 
 		
-		if ((this.type != Type.BEFORE) && (this.allowSqueeze || tokenSpan.getEndTokenIndex() + this.n <= tokenSpan.getDocument().getSentenceTokenCount(tokenSpan.getSentenceIndex()))) {
+		if ((this.type != Type.BEFORE && this.type != Type.BEFORE_INCLUDING) && (this.allowSqueeze || tokenSpan.getEndTokenIndex() + this.n <= tokenSpan.getDocument().getSentenceTokenCount(tokenSpan.getSentenceIndex()))) {
 			ngrams.add(new TokenSpan(tokenSpan.getDocument(), 
 					 				 tokenSpan.getSentenceIndex(), 
 					 				 tokenSpan.getEndTokenIndex() - ((this.type == Type.AFTER || this.type == Type.BEFORE_AND_AFTER) ? 0 : tokenSpan.getLength()), 
